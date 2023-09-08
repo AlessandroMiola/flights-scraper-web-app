efc3e86862e50f32b262a5f7ccc000138940c06c
AlessandroMiola <alessandro1.miola@gmail.com>
2023-09-09 00:51:46 +0200

    WIP: One HTML form only to process necessary and optional FE data with a single route

--- src/apps/v1/render_templates.py
+++ src/apps/v1/render_templates.py
@@ -21,11 +21,11 @@ def func_name(
     trip_type: str = Form(...),
     departure_location: str = Form(...),
     arrival_location: str = Form(...),
-    # departure_location_comeback: Optional[str] = Form(),
-    # arrival_location_comeback: Optional[str] = Form(),
+    departure_location_comeback: Optional[str] = Form(None),
+    arrival_location_comeback: Optional[str] = Form(None),
 ):
     print(f"Trip type is {trip_type}")
     print(f"Departure from {departure_location}")
     print(f"Arrival to {arrival_location}")
-    # print(f"Departure (comeback) from {departure_location_comeback}")
-    # print(f"Arrival (comeback) to {arrival_location_comeback}")
+    print(f"Departure (comeback) from {departure_location_comeback}")
+    print(f"Arrival (comeback) to {arrival_location_comeback}")
